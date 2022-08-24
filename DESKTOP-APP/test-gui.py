d77299175c0d1b9095f92fae6a36c309dbf990c5
KayChewe <chewe.kasonde@outlook.com>
2022-08-24 09:49:03 +0200

    bls-data module test

---
+++ DESKTOP-APP/test-gui.py
@@ -0,0 +1,15 @@
+"""
+    file: test-gui.py
+    author: Kasonde Chew
+    
+    Program is basic GUI interface test for the Hunter Forex Finance Application
+    
+    Modules (experimental)
+    1. Fundamental Data 
+        bls-data 
+        
+    2. Technical Data
+        yfinance 
+        
+        
+"""
